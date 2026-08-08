b034883d686f68ad27369bc2c2f033a4521a6cd4
lynx lee <3227560@gmail.com>
2026-08-08 12:36:41 +0800

    fix(moonshot): add kimi-k2.7-code-highspeed to model list

--- relay/channel/moonshot/constants.go
+++ relay/channel/moonshot/constants.go
@@ -4,6 +4,7 @@ var ModelList = []string{
 	// Current models per https://platform.kimi.com/docs/api/chat
 	"kimi-k3",
 	"kimi-k2.7-code",
+	"kimi-k2.7-code-highspeed",
 	"kimi-k2.6",
 	"kimi-k2.5",
 	"moonshot-v1",
